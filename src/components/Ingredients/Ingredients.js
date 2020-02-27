a278f4cb75b79582e34855a6448dc40ceab9761a
TyAssis <tyassis3@gmail.com>
2020-02-27 09:48:16 -0300

    Add crud functions to project

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,15 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+
+  const [ ingredients, setIngredients ] = useState([]);
+
+  // not needed anymore, once it is fetched on Search
+  // useEffect(() => {
+  //   fetch('https://build-burguer.firebaseio.com/ingredients-2.json')
+  //   .then(response =>  response.json())
+  //   .then(body => {
+  //     const loadedIngredients = [];
+  //     for (const key in body) {
+  //       loadedIngredients.push({
+  //         id: key,
+  //         title: body[key].title,
+  //         amount: body[key].amount,
+  //       })
+  //     }
+  //     setIngredients(loadedIngredients)
+  //   })
+  // }, []);
+
+
+  const addIngredientHandler = ingredient => {
+    fetch('https://build-burguer.firebaseio.com/ingredients-2.json', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify({
+        ...ingredient
+      })
+    }).then(response =>
+        response.json()
+      ).then(body => {
+        setIngredients(prevIngredients => [ 
+          ...prevIngredients, 
+          { 
+            id: body.name,
+            ...ingredient, 
+          } 
+        ])
+      })
+  };
+
+  const removeIngredientHandler = id => {
+    setIngredients(prevIngredients => 
+      prevIngredients.filter(prevIngredient => prevIngredient.id !== id)
+    );
+  };
+
+  const filterIngredientsHandler = useCallback(filteredIngredients => {
+    setIngredients(filteredIngredients);
+  }, []);
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadingIngredients={filterIngredientsHandler} />
+        <IngredientList ingredients={ingredients} onRemoveItem={removeIngredientHandler} />
         {/* Need to add list here! */}
       </section>
     </div>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const { onLoadingIngredients } = props;
+  const [inputFiler, setInputFilter ] = useState('');
+  const inputRef = useRef();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (inputFiler === inputRef.current.value) {
+        const queryParams = inputFiler.length === 0 ? '' : `?orderBy="title"&equalTo="${inputFiler}"`;
+        fetch(`https://build-burguer.firebaseio.com/ingredients-2.json${queryParams}`)
+        .then(response =>  response.json())
+        .then(body => {
+          const loadedIngredients = [];
+          for (const key in body) {
+            loadedIngredients.push({
+              id: key,
+              title: body[key].title,
+              amount: body[key].amount,
+            })
+          } 
+          onLoadingIngredients(loadedIngredients);
+        });
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    }
+  },
+    [inputFiler, onLoadingIngredients, inputRef]
+  );
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input type="text" 
+            value={inputFiler}
+            ref={inputRef}
+            onChange={event => setInputFilter(event.target.value) }  
+          />
         </div>
       </Card>
     </section>
